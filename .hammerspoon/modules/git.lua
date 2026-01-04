0f72ea4dda590a8c04cab2933901fe70553607fc
ohashi yuki <ohashiyuki@ohashinoMacBook-Air.local>
2026-01-05 08:15:37 +0900

    auto

--- .hammerspoon/modules/git.lua
+++ .hammerspoon/modules/git.lua
@@ -1,6 +1,21 @@
 
-hs.timer.doEvery(20, function()
-    -- シムリンクの実体の親ディレクトリ（dotfiles）を取得して、そこでgitを実行
-    local cmd = "target=$(readlink " .. hs.configdir .. " || echo " .. hs.configdir .. ") && cd $(dirname $target) && git add . && git commit -m 'auto' && git push"
-    hs.execute(cmd)
+hs.timer.doEvery(30, function()
+        hs.alert.show("test")
+    -- システムの標準的な場所から git を探す
+    local git = hs.execute("export PATH=$PATH:/usr/local/bin:/opt/homebrew/bin; which git"):gsub("\n", "")
+    local repo = os.getenv("HOME") .. "/dotfiles"
+
+    if git == "" then
+        hs.alert.show("Git Error: git command not found.")
+        return
+    end
+
+    -- 実行（認証エラーを防ぐため PATH を引き継ぐ）
+    local cmd = string.format("export PATH=$PATH:/usr/local/bin:/opt/homebrew/bin; cd %s && %s add . && %s commit -m 'auto' && %s push", repo, git, git, git)
+
+    local output, status = hs.execute(cmd)
+
+    if not status then
+        hs.alert.show("Git Push Error:", output)
+    end
 end):start()
