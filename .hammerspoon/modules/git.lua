a4f630bac6b8a30f14fa8e9de58ddbc18056b1d7
ohashi yuki <ohashiyuki@ohashinoMacBook-Air.local>
2026-01-05 08:33:01 +0900

    auto commit: 2026/01/05 08:33:01

--- .hammerspoon/modules/git.lua
+++ .hammerspoon/modules/git.lua
@@ -1,13 +1,16 @@
-local gitTimer = hs.timer.doEvery(5, function()
+-- 3分に一度、git commitとpushをする
+local gitTimer = hs.timer.doEvery(3 * 60, function()
     local repo = os.getenv("HOME") .. "/dotfiles"
+    local timestamp = os.date("%Y/%m/%d %H:%M:%S")
+    local commitMsg = string.format("auto commit: %s", timestamp)
 
     local cmd = string.format([[
-        cd %s && git add . && git commit -m 'auto' && git push
-    ]], repo)
+        cd %s && git add . && git commit -m '%s' && git push
+    ]], repo, commitMsg)
 
     local output, status = hs.execute(cmd)
 
-    if not status then
+    if not status and not string.match(output or "", "nothing to commit") then
         print("Git error:", output)
     end
 end):start()
