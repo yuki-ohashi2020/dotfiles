89d14b3a1d8ce423f3da5a66249de84660d8d45a
yuki-ohashi2020 <yuki.ohashi2020@gmail.com>
2026-01-05 10:17:08 +0900

    auto commit: 2026/01/05 10:17

---
+++ .hammerspoon/modules/config.lua
@@ -0,0 +1,10 @@
+APP_BINDINGS = {
+  o = {
+    name = "Obsidian",
+    frame = { x = 100, y = 100, w = 1200, h = 10 }
+  },
+  v = {
+    name = "Vivaldi",
+    frame = { x = 50, y = 50, w = 1400, h = 900 }
+  },
+}
