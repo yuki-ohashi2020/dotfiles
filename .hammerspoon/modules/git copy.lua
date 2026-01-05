57e44b90f7851401b6f2cd5da5465a4da1bc1d3c
yuki-ohashi2020 <yuki.ohashi2020@gmail.com>
2026-01-05 09:59:44 +0900

    auto commit: 2026/01/05 09:59

---
+++ .hammerspoon/modules/git copy.lua
@@ -0,0 +1,27 @@
+-- ==========================================
+-- auto commit & Push
+-- ==========================================
+-- 3分間隔
+local Interval = 3 * 60
+
+local function autoGitSync()
+    local repo = os.getenv("HOME") .. "/dotfiles"
+    local timestamp = os.date("%Y/%m/%d %H:%M")
+    local commitMsg = string.format("auto commit: %s", timestamp)
+
+    local cmd = string.format([[
+        cd %s && git add . && git commit -m '%s' && git push
+    ]], repo, commitMsg)
+
+    local output, status = hs.execute(cmd)
+
+    if not status and not string.match(output or "", "nothing to commit") then
+        print("Git error:", output)
+    end
+end
+
+-- 1. 初回起動時に実行（差分があれば即コミット）
+autoGitSync()
+
+-- 2. 以降、3分間隔で実行
+local gitTimer = hs.timer.doEvery(Interval, autoGitSync):start()
