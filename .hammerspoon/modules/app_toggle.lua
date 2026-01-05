f236473e031e14d121f29df1f970edd3e687ebb2
yuki-ohashi2020 <yuki.ohashi2020@gmail.com>
2026-01-05 10:08:25 +0900

    auto commit: 2026/01/05 10:08

--- .hammerspoon/modules/app_toggle.lua
+++ .hammerspoon/modules/app_toggle.lua
@@ -3,3 +3,35 @@ hs.hotkey.bind({"ctrl","alt"}, "o", function()
     x = 100, y = 100, w = 1200, h = 800
   })
 end)
+
+local function toggleApp(appName, frame)
+  local app = hs.application.get(appName)
+
+  -- 起動していなければ起動
+  if not app then
+    hs.application.launchOrFocus(appName)
+    hs.timer.doAfter(0.5, function()
+      local a = hs.application.get(appName)
+      if a then
+        local win = a:mainWindow()
+        if win and frame then
+          win:setFrame(frame)
+        end
+      end
+    end)
+    return
+  end
+
+  -- 表示中なら非表示
+  if app:isFrontmost() then
+    app:hide()
+    return
+  end
+
+  -- 非表示なら表示
+  app:activate(true)
+  local win = app:mainWindow()
+  if win and frame then
+    win:setFrame(frame)
+  end
+end
