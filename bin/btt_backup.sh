46a447844f9743cbca70255a829694dab9466698
yuki-ohashi2020 <yuki.ohashi2020@gmail.com>
2026-01-19 08:59:21 +0900

    auto commit: 2026-01-19 08:59:21

--- bin/btt_backup.sh
+++ bin/btt_backup.sh
@@ -8,15 +8,10 @@
 # 1. バックアップ元（BTTのデータフォルダ）
 SOURCE_DIR="$HOME/Library/Application Support/BetterTouchTool"
 BACKUP_DIR="BTT"
-# 2. iCloud内の保存先
-# BACKUP_DIR="$HOME/Library/Mobile Documents/com~apple~CloudDocs/Backups/BTT"
-# 3. 保持する世代数
+# 保持する世代数
 KEEP_COUNT=5
 # 4. バックアップファイル名のプレフィックス
 FILE_PREFIX="BTT_Full_Backup"
-# 5. バックアップ後のファイル名 (形式: 2026_01_17_07:40:01)
-# DATE=$(date +%Y_%m_%d_%H:%M:%S)
-# DEST_FILE="$BACKUP_DIR/${FILE_PREFIX}_$DATE.zip"
 # ----------------
 
 SOURCE_DIR="$SOURCE_DIR" \
