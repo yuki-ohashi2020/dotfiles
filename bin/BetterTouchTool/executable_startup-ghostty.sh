ec2fb3a22af31083f679eb23eb715215bcdc7a39
yuki-ohashi2020 <yuki.ohashi2020@gmail.com>
2026-01-22 10:35:20 +0900

    auto commit: 2026-01-22 10:35:20

--- bin/BetterTouchTool/executable_startup-ghostty.sh
+++ bin/BetterTouchTool/executable_startup-ghostty.sh
@@ -4,8 +4,9 @@
 NVIM_PATH="/opt/homebrew/bin/nvim"
 TARGET_FILE="$HOME/Working_Note"
 
-# Ghosttyが起動していない場合
-if ! pgrep -x "Ghostty" > /dev/null; then
-   # Ghosttyを新規起動してnvimで特定ファイルを開く
-    open -na Ghostty --args -e zsh -c "$NVIM_PATH '$TARGET_FILE'; exit"
+# Ghosttyが起動していない場合のみ実行
+if ! pgrep -q "Ghostty"; then
+  # バックグラウンドで起動（最前面に出さない）
+  # 表示/非表示の切り替えや表示位置の制御はBTT側で行うこと
+  open -jga Ghostty --args -e zsh -c "$NVIM_PATH '$TARGET_FILE'; exit"
 fi
