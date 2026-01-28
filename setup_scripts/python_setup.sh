21242f316cf60beadb6f710bb93b4fa8b693e97e
yuki <yuki@yukinoMac-mini.local>
2026-01-28 15:54:55 +0900

    auto commit: 2026-01-28 15:54:55

--- setup_scripts/python_setup.sh
+++ setup_scripts/python_setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+PYTHON_VERSION=3.12.2
+
 #  pyenv コマンドにパスが通っているか確認
 if ! command -v pyenv >/dev/null 2>&1; then
   echo "Error: Pyenv is not installed or not in your PATH." >&2
@@ -8,3 +10,4 @@ fi
 
 # -s インストール済みの場合はスキップ
 pyenv install -s "$PYTHON_VERSION"
+pyenv global "$PYTHON_VERSION"
